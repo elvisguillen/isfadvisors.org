6e3bbecb13561a0cd754f21344e9650495527a9c
spoke-dev <elvisguillen@elviss-mbp.lan>
2018-03-08 16:47:17 -0500

    Created careers page template

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,6 @@ import { Container, Row, Col, Card, CardText, CardBody, CardTitle, CardSubtitle 
 import PageHero from '../components/PageHero'
 import PageContent from '../components/PageContent'
 import PageAltContent from '../components/PageAltContent'
-import MediaCoverage from '../components/MediaCoverage'
 import PageCTA from '../components/PageCTA'
 
 const AboutPage = ({transition}) => (
@@ -14,34 +13,30 @@ const AboutPage = ({transition}) => (
     </PageHero>
 
     <PageContent
-      left={
-        <Link to='/team'><button>Team</button></Link>
-      }
-      right={
-        <div>
-          <h3>ISF is housed within the Global Development Incubator (GDI) and shares GDI’s commitment to building effective partnerships that make a significant and lasting impact. GDI is an organization that builds startups, incubates partnerships, and strengthens existing organizations for social impact around the world.</h3>
-          <p>
-            ISF’s advisory committee consists of leading funders and practitioners in the rural financial inclusion space, specifically ANDE, Bill and Melinda Gates Foundation, Business Fights Poverty, CGAP, Citi Foundation, Ford Foundation, IDH, MasterCard Foundation, One Acre Fund, Root Capital, Skoll Foundation, Small Foundation, TechnoServe, and USAID. 
-          </p>
-          <p>
-            ISF has completed projects with a wide variety of public and private sector partners, including Airtel, Bill and Melinda Gates Foundation, Council on Smallholder Agricultural Finance, Dalberg Global Development Advisors, Esoko, IDH Sustainable Trade Initiative, International Fund for Agricultural Development (IFAD), KfW/DEG, Komaza, MIX Market, Opportunity International, Partners in Food Solutions, Rabobank, Rockefeller Foundation, TechnoServe, USAID, Vodacom, World Cocoa Foundation, and World Food Programme. 
-          </p>
-          <p>
-            ISF regularly co-sponsors research and publishes insights with the Rural and Agricultural Finance Learning Lab.
-          </p>
-        </div>
-      } />
+    left={
+      <Link to='/team'><button>Team</button></Link>
+    }
+    right={
+      <div>
+        <h3>ISF is housed within the Global Development Incubator (GDI) and shares GDI’s commitment to building effective partnerships that make a significant and lasting impact. GDI is an organization that builds startups, incubates partnerships, and strengthens existing organizations for social impact around the world.</h3>
+        <p>
+          ISF’s advisory committee consists of leading funders and practitioners in the rural financial inclusion space, specifically ANDE, Bill and Melinda Gates Foundation, Business Fights Poverty, CGAP, Citi Foundation, Ford Foundation, IDH, MasterCard Foundation, One Acre Fund, Root Capital, Skoll Foundation, Small Foundation, TechnoServe, and USAID. 
+        </p>
+        <p>
+          ISF has completed projects with a wide variety of public and private sector partners, including Airtel, Bill and Melinda Gates Foundation, Council on Smallholder Agricultural Finance, Dalberg Global Development Advisors, Esoko, IDH Sustainable Trade Initiative, International Fund for Agricultural Development (IFAD), KfW/DEG, Komaza, MIX Market, Opportunity International, Partners in Food Solutions, Rabobank, Rockefeller Foundation, TechnoServe, USAID, Vodacom, World Cocoa Foundation, and World Food Programme. 
+        </p>
+        <p>
+          ISF regularly co-sponsors research and publishes insights with the Rural and Agricultural Finance Learning Lab.
+        </p>
+      </div>
+    } />
 
     <PageAltContent title='Media Coverage'>
-
-        <MediaCoverage/>
-      
-     
     </PageAltContent>
 
     <PageCTA>
       <div>
-        <p>Check out our <Link to='/'>case studies</Link> & industry-leading <Link to='/'>research.</Link></p>
+        <p>Check out our <Link to='/'>case studies</Link>, industry-leading <Link to='/'>research.</Link></p>
         <p>You can also <Link to='/'>contact us</Link> or sign up for our <Link to='/'>newsletter.</Link></p>
       </div>
     </PageCTA>
---
+++ src/pages/careers.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import { Container, Row, Col, Card, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap'
+import PageHero from '../components/PageHero'
+import PageContent from '../components/PageContent'
+import PageAltContent from '../components/PageAltContent'
+import MediaCoverage from '../components/MediaCoverage'
+import PageCTA from '../components/PageCTA'
+
+const CareerPage = ({transition}) => (
+  <div style={transition && transition.style}>
+    <PageHero>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </PageHero>
+
+    <PageContent
+      right={
+        <div>
+          <h3>ISF currently does not have any job openings. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</h3>
+        </div>
+      } />
+
+    <PageCTA>
+      <div>
+        <p>Check out our <Link to='/'>case studies</Link> & industry-leading <Link to='/'>research.</Link></p>
+        <p>You can also <Link to='/'>contact us</Link> or sign up for our <Link to='/'>newsletter.</Link></p>
+      </div>
+    </PageCTA>
+  </div>
+)
+
+export default CareerPage
