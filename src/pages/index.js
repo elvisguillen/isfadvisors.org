3de83c96f02b242f4a9129e0a9fcc44217e9d5de
spoke-dev <elvisguillen@Elviss-MacBook-Pro.local>
2018-05-25 13:06:01 -0400

    Adjusted spacing, font sizes, margins, added scroll arrow, responsive spacing and font sizing edits, removed animations from mobile, removed fade animations between pages on all devices, added dynamic team page feed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,84 +1,106 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Link from 'gatsby-link'
 import { Container, Row, Col, Card, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap'
 import Reveal from 'react-reveal/Reveal'
 import CaseStudies from '../components/CaseStudies'
+import { Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
 import intro_photo from '../images/intro_photo.jpg'
+import iconScrollArrow from '../images/icon_scroll_arrow.png'
 
-const IndexPage = ({transition}) => (
-  <div style={transition && transition.style}>
-    <section className='hero'>
-      <Container>
-        <h1 className='text-center'><span>We elevate rural enterprises by delivering partnerships and investment structures that promote financial inclusion for smallholders and rural economies.</span></h1>
-      </Container>
-    </section>
+class IndexPage extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.handleScrollClick = this.handleScrollClick.bind(this)
+  }
 
-    <section className='intro'>
-      <Container>
-        <Row>
-          <Col xs='12' lg={{size: 6, offset: 0}} >
+  handleScrollClick(e) {
+    scroll.scrollTo(800, {
+      duration: 1000,
+      smooth: "easeOutCubic"
+    });
+  }
+
+  render() {
+    return (
+      <div style={this.props.transition && this.props.transition.style}>
+        <section className='hero'>
+          <Container>
+            <h1 className='text-center'><span>We elevate rural enterprises by delivering partnerships and investment structures that promote financial inclusion for smallholders and rural economies.</span></h1>
+            <div className='scroll-button' onClick={this.handleScrollClick}></div>
+          </Container>
+        </section>
+
+        <Element name='Intro'>
+          <section className='intro'>
+            <Container>
+              <Row>
+                <Col xs='12' lg={{size: 6, offset: 0}} >
+                  <Reveal effect='fadeInUp'>
+                    <p>Combining industry leading research with hands-on technical expertise, ISF develops practical, profitable, and sustainable financial solutions.</p>
+                    <Link to='/services'><button>Services</button></Link>
+                  </Reveal>
+                </Col>
+                <Col xs='12' lg={{size: 6, offset: 0}} xl={{size: 5, offset: 1}} className='intro_photo'>
+                  <Reveal effect='fadeInUp'>
+                    <img className='img-fluid d-none d-lg-block' src={intro_photo} />
+                  </Reveal>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+        </Element>
+        <img className='img-fluid d-lg-none' src={intro_photo} />
+        <section className='rural-farmers home text-center'>
+          <Container>
             <Reveal effect='fadeInUp'>
-              <p>Combining industry leading research with hands-on technical expertise, ISF develops practical, profitable, and sustainable financial solutions.</p>
-              <Link to='/services'><button>Services</button></Link>
+            <Col lg={{size: 8, offset: 2}} >
+              <h1 className='text-center'><span>Why rural enterprise and smallholder farmers?</span></h1>
+            </Col>
             </Reveal>
-          </Col>
-          <Col xs='12' lg={{size: 6, offset: 0}} xl={{size: 5, offset: 1}} className='intro_photo'>
+            <div className='rural-farmers-reasons'> 
+              
             <Reveal effect='fadeInUp'>
-              <img className='img-fluid d-none d-lg-block' src={intro_photo} />
+              <Row>
+                  <Col xs='12' lg='4' className='reason'>
+                    <h2>Food <br/>Security</h2>
+                    <p>for a world population of 9 billion by 2050</p>
+                  </Col>
+                  <Col xs='12' lg='4' className='reason'>
+                    <h2>Poverty Alleviation</h2>
+                    <p>for the 2 billion dependant on agriculture for income</p>
+                  </Col>
+                  <Col xs='12' lg='4' className='reason'>
+                    <h2>Land Preservation</h2>
+                    <p>for the protection of natural resources</p>
+                  </Col>
+              </Row>
             </Reveal>
-          </Col>
-        </Row>
-      </Container>
-    </section>
-    <img className='img-fluid d-lg-none' src={intro_photo} />
-    <section className='rural-farmers home text-center'>
-      <Container>
-        <Reveal effect='fadeInUp'>
-        <Col lg={{size: 8, offset: 2}} >
-          <h1 className='text-center'><span>Why rural enterprise and smallholder farmers?</span></h1>
-        </Col>
-        </Reveal>
-        <div className='rural-farmers-reasons'> 
-          
-        <Reveal effect='fadeInUp'>
-          <Row>
-              <Col xs='12' lg='4' className='reason'>
-                <h2>Food <br/>Security</h2>
-                <p>for a world population of 9 billion by 2050</p>
-              </Col>
-              <Col xs='12' lg='4' className='reason'>
-                <h2>Poverty Alleviation</h2>
-                <p>for the 2 billion dependant on agriculture for income</p>
-              </Col>
-              <Col xs='12' lg='4' className='reason'>
-                <h2>Land Preservation</h2>
-                <p>for the protection of natural resources</p>
-              </Col>
-          </Row>
-        </Reveal>
-        </div>
-      </Container>
-    </section>
+            </div>
+          </Container>
+        </section>
 
-    <CaseStudies />
-    
-    <section className='cta text-center'>
-      <Container>
-        <Row>
-          <Col xs='12' md='2' lg='2'>
-            <img className='img-fluid' src='/images/footer-icon.png' />
-          </Col>
-          <Col xs='12' md='10' lg='9'>
-            <p className='text-left'>
-              Learn more about our <Link to='/services#pageAltContent'>areas of expertise</Link> & industry-leading <Link to='/research'>research</Link> or current <Link to='/careers'>job openings.</Link> You can also <Link to='/contact'>contact us</Link> or sign up for our <Link to='/contact'>newsletter.</Link>
-            </p>
-          </Col>
-        </Row>
-      </Container>
-    </section>
-    
-  </div>
-)
+        <CaseStudies />
+        
+        <section className='cta text-center'>
+          <Container>
+            <Row>
+              <Col xs='12' md='2' lg='2'>
+                <img className='img-fluid' src='/images/footer-icon.png' />
+              </Col>
+              <Col xs='12' md='10' lg='9'>
+                <p className='text-left'>
+                  Learn more about our <Link to='/services#pageAltContent'>areas of expertise</Link> & industry-leading <Link to='/research'>research</Link> or current <Link to='/careers'>job openings.</Link> You can also <Link to='/contact'>contact us</Link> or sign up for our <Link to='/contact'>newsletter.</Link>
+                </p>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+        
+      </div>
+    )
+  }
+}
 
 export default IndexPage
---
+++ src/templates/team.js
@@ -0,0 +1,149 @@
+import React, {Component} from 'react'
+import Link from 'gatsby-link'
+import { Container, Row, Col, Card, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap'
+import PageHero from '../components/PageHero'
+import PageContent from '../components/PageContent'
+import PageAltContent from '../components/PageAltContent'
+import PageCTA from '../components/PageCTA'
+import TeamMember from '../components/TeamMember'
+
+import team_matt from '../images/Matthew_Shakhovsky.jpg'
+
+import hero_page from '../images/hero_page.jpg'
+
+// const Team = [
+//     {
+//         name: 'Matt Shakhovskoy',
+//         title: 'Executive Director',
+//         img: '/images/team/Matt.jpg'
+//     },
+//     {
+//         name: 'Dan Zook',
+//         title: 'Director of Investments',
+//         img: '/images/team/Dan.jpg'
+//     },
+//     {
+//         name: 'Tom Carroll',
+//         title: 'Senior Advisor and Founder',
+//         img: '/images/team/Tom.jpg'
+//     },
+//     {
+//         name: 'Craig Courtney',
+//         title: 'Senior Advisor',
+//         img: '/images/team/Craig.jpg'
+//     },
+//     {
+//         name: 'Elizabeth Lara',
+//         title: 'Senior Manager',
+//         img: '/images/team/Liz.jpg'
+//     },
+//     {
+//         name: 'Martin Slawek',
+//         title: 'Manager',
+//         img: '/images/team/Martin.jpg'
+//     },
+//     {
+//         name: 'Romit Mehta',
+//         title: 'Manager',
+//         img: '/images/team/Placeholder.jpg'
+//     },
+//     {
+//         name: 'Malia Bachesta Eley',
+//         title: 'Communications Associate',
+//         img: '/images/team/Malia.jpg'
+//     },
+// ]
+
+class TeamPage extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        const page = this.props.data.wordpressPage
+        const Team = this.props.data.allWordpressWpTeam
+        
+        return(
+            <div style={this.transition && this.transition.style}>
+                <PageHero heroImage={hero_page}>
+                    <span className='background-span-blue' dangerouslySetInnerHTML={{ __html: page.acf.page_header_copy}} />
+                </PageHero>
+
+                <PageContent
+                left={
+                    <Link to='/about'><button>Back</button></Link>
+                }
+                right={
+                    <div dangerouslySetInnerHTML={{__html: page.content}}/>
+                } />
+
+                <PageAltContent title='Team'>
+                    <Row>
+                        {Team.edges.map(({node: member}, i) => {
+                            return (    
+                                <TeamMember 
+                                    key={i}
+                                    thumbnail={
+                                        <img className='img-fluid' src={member.featured_media.source_url} />
+                                    }
+                                    link={member.title.replace(/\s+/g, '-').toLowerCase()}
+                                    name={member.title}
+                                    title={member.acf.job_title} />
+                            )
+                        })}
+                    </Row>
+                    
+                    
+                </PageAltContent>
+
+                <PageCTA>
+                <div>
+                    <p>Check out our <Link to='/'>case studies</Link> & industry-leading <Link to='/'>research.</Link></p>
+                    <p>You can also <Link to='/'>contact us</Link> or sign up for our <Link to='/'>newsletter.</Link></p>
+                </div>
+                </PageCTA>
+            </div>
+        )
+    }
+}
+
+export default TeamPage
+
+export const teamPageQuery = graphql`
+  query TeamPageQuery {
+    wordpressPage(slug: { eq: "team" }) {
+        id
+        slug
+        title
+        content
+        excerpt
+        date
+        modified
+        acf {
+            intro_copy
+            main_copy
+            page_header_copy
+        }
+    }
+    
+    allWordpressWpTeam(sort: { order: ASC, fields: [date] },) {
+        edges {
+            node{
+                id
+                slug
+                title
+                content
+                date
+                modified
+              	featured_media {
+                  source_url
+                }
+                acf {
+                  job_title
+                }
+            }
+        }
+    } 
+  }
+`
